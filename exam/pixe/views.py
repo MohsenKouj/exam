6399c3b596d9aeade2b19fd4d30b569561f908be
MohsenKouj <misionorido@gmail.com>
2024-06-27 12:31:37 +0430

    summery

--- exam/pixe/views.py
+++ exam/pixe/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.http import Http404,HttpResponseNotFound
 from .models import *
 import datetime as dt
 
 now = dt.datetime.now()
 class dataAdapt:
+    context = dict(post = list())
     def __init__(self,id,publisher,date_modified,c_views,title,disc):
         self.id = id
         self.publisher = publisher
@@ -11,15 +13,16 @@ class dataAdapt:
         self.c_view = c_views
         self.title = title
         self.disc = disc
-    
+
     def delete(self):
         del self.id
         del self.publisher
         del self.date_modified
-        del self.c_views
+        del self.c_view
         del self.title
         del self.disc
-        
+ 
+       
 # Create your views here.
 def post(request):
     #rdate = now.year + now.month + now.day + now.hour + now.minute + now.second + now.microsecond
@@ -42,5 +45,11 @@ def single(request,ids):
     p = Post.objects.get(id=ids)
     p.c_view += 1
     p.save()
-    context = {'post':Post.objects.filter(id=ids)}
-    return render(request, 'single.html', context)
+    if now.timestamp() > p.date_modified.timestamp():
+        context = {'post':Post.objects.get(id=ids)}
+        return render(request, 'single.html', context)
+    else:
+        
+        return HttpResponseNotFound(request,'No such post')
+        
+        
