ae63764e0736050bbe074c8ca67ab664db78bb92
MohsenKouj <misionorido@gmail.com>
2024-06-29 11:44:30 +0430

    late

--- exam/pixe/urls.py
+++ exam/pixe/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 from pixe.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('',post,name='post'),
     path('single/s-<int:ids>',single,name='single')
 ]
-
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
